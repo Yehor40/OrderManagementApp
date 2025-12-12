81c00ed1b2b68dec7d343fd3a0e28a6d85b7e4a6
Yehor40 <fraugrey3@gmail.com>
2025-12-12 23:11:25 +0100

    additional commit

---
+++ OrderManagementApp/Migrations/20251212173624_InitialCreate.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace OrderManagementApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Statuses",
+                columns: table => new
+                {
+                    Code = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    StatusName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statuses", x => x.Code);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    StatusCode = table.Column<string>(type: "nvarchar(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_Statuses_StatusCode",
+                        column: x => x.StatusCode,
+                        principalTable: "Statuses",
+                        principalColumn: "Code",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "CustomerName", "Email", "Password" },
+                values: new object[] { 1, "John Doe", "john@example.com", "hashed_password" });
+
+            migrationBuilder.InsertData(
+                table: "Statuses",
+                columns: new[] { "Code", "StatusName" },
+                values: new object[,]
+                {
+                    { "C", "Canceled" },
+                    { "P", "Processing" },
+                    { "S", "Shipped" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "OrderId", "Amount", "CustomerId", "CustomerName", "Date", "StatusCode" },
+                values: new object[] { 1, 1500, 1, "John Doe", new DateTime(2025, 12, 12, 18, 36, 23, 897, DateTimeKind.Local).AddTicks(9340), "P" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_StatusCode",
+                table: "Orders",
+                column: "StatusCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Statuses");
+        }
+    }
+}
---
+++ OrderManagementApp/Migrations/20251212202059_Customerseed.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace OrderManagementApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Customerseed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_StatusCode",
+                table: "Orders");
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "CustomerName", "Email", "Password" },
+                values: new object[,]
+                {
+                    { 2, "Jane Smith", "jane@example.com", "hashed_password" },
+                    { 3, "Acme Corporation LLC", "contact@acme.com", "hashed_password" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2025, 12, 12, 21, 20, 59, 605, DateTimeKind.Local).AddTicks(870));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_StatusCode",
+                table: "Orders",
+                column: "StatusCode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_StatusCode",
+                table: "Orders");
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "CustomerId",
+                keyValue: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2025, 12, 12, 18, 36, 23, 897, DateTimeKind.Local).AddTicks(9340));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_StatusCode",
+                table: "Orders",
+                column: "StatusCode",
+                unique: true);
+        }
+    }
+}
